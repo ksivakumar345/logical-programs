438e1330e1e26b37b65f1e202db1d5607b1e7e58
ksivakumar345 <108319722+ksivakumar345@users.noreply.github.com>
2022-07-14 13:37:37 +0530

    Add files via upload

--- BooleanOperation.java
+++ BooleanOperation.java
@@ -1,51 +1,33 @@
-/*  JFM1T2_Assignment3:
+//JFM1T3_Assignment5:
+/*
+1.Write a program to initialize a boolean variable and print it's opposite value on console.
 
-     Print the results for the following statements considering x = true and y = false
-             1. x&&y
-             2. x||y
-             3. !x
-             4. !y
-     Prompt the user input from the terminal.
-             
-     Sample Input:
-     Enter x as boolean value(true/false): 
-     true
-     Enter y as boolean value(true/false): 
-     false
-     
-     Expected Output:
-     x&&y is: false
-     x||y is: true
-     !x is: false
-     !y is: true
+  Sample Input:true
+
+  Expected Output:false
 
 */
-
-//import statements for java program to read inputs using Scanner class
-import java.util.Scanner;
-
 public class BooleanOperation {
 
-//main method
-public static void main(String args[]){
-  Scanner sc=new Scanner(System.in);
-   System.out.println("Enter x as boolean value(true/false): ");
-  boolean x=sc.nextBoolean();
-   System.out.println("Enter y as boolean value(true/false): ");
-  boolean y=sc.nextBoolean();
-  System.out.println(x&&y);
-    System.out.println(x||y);
-    System.out.println(!x);
-    System.out.println(!y);
-    
-  
-/*Use the scanner class to provide input at execution time using scanner object
-  Scanner sc=new Scanner(System.in);
-*/
-//Take input from user  System.out.println("Enter *x as boolean value(true/false): ");
-  //boolean x=sc.nextBoolean();
+//Define main method
+public static void main(String args[])
+{
+
+//Declare a variable and initialize it as true or false 
+boolean user=true;
+if(!user)
+{
+  System.out.println("true");
+}
+else
+{
+//Print the Result using not operator 
+System.out.println("false");
 
 }
-//print apppropriate result for boolean operations
-
 }
+}
+//Declare a variable and initialize it as true or false 
+
+//Print the Result using not operator 
+
